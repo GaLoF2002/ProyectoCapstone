4f004b0c87c05c80d4ee9547ee609b889834a07f
GaloAndresE <galo.estrella.jimenez@udla.edu.ec>
2025-06-30 15:05:41 -0500

    Deploy de la aplicación prueba 4.9

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,24 +1,27 @@
 import axios from 'axios';
 
-// Después (funciona en local y producción)
+// ✅ Usar VITE_API_URL asegura que funcione en local y en Heroku
 const API_URL = import.meta.env.VITE_API_URL + "/auth";
 
-
+// Registrar usuario
 export const register = async (userData) => {
     return await axios.post(`${API_URL}/register`, userData);
 };
 
+// Login
 export const login = async (userData) => {
     return await axios.post(`${API_URL}/login`, userData);
 };
 
+// Enviar email de recuperación
 export const enviarEmailRecuperacion = (email) => {
     return axios.post(`${API_URL}/forgot-password`, { email });
 };
 
+// Resetear contraseña
 export const resetearContrasena = async (token, password) => {
     return await axios.post(`${API_URL}/reset-password/${token}`, {
-        password, // ✅ Se envía solo la nueva contraseña en el body
+        password
     }, {
         headers: {
             "Content-Type": "application/json"
